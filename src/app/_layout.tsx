d916aeb6e37daf0f9a7bfe45df3f705b56100441
Kasun Vithanage <alankasun@gmail.com>
2024-09-10 22:41:07 +0530

    feat: basic tanstack hooks

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,44 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  focusManager,
+} from "@tanstack/react-query";
+import { useReactQueryDevTools } from "@dev-plugins/react-query";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { AppState, Platform } from "react-native";
+import type { AppStateStatus } from "react-native";
+import NetInfo from "@react-native-community/netinfo";
+import { onlineManager } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+onlineManager.setEventListener((setOnline) => {
+  return NetInfo.addEventListener((state) => {
+    setOnline(!!state.isConnected);
+  });
+});
+
+function onAppStateChange(status: AppStateStatus) {
+  if (Platform.OS !== "web") {
+    focusManager.setFocused(status === "active");
+  }
+}
 
 export default function RootLayout() {
+  useReactQueryDevTools(queryClient);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", onAppStateChange);
+
+    return () => subscription.remove();
+  }, []);
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="index" />
+      </Stack>
+    </QueryClientProvider>
   );
 }
